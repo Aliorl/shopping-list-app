8f76e5773494db60d489bbf9212e758c1e996c62
Alicja Orlik <alicja.k.orlik@gmail.com>
2023-01-06 20:22:54 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+  <div id="app">
+    <h1>Shopping List App</h1>
+    <shoppingListApp msg="Welcome to Shopping List App" />
+  </div>
+</div>
+</template>
+
+<script>
+import shoppingList from './components/shoppingListApp.vue'
+
+export default {
+  name: 'app',
+  components: {
+    shoppingListApp: shoppingList
+  }
+}
+</script>
+
+<style>
+@import '/src/assets/style.css';
+</style>
---
+++ src/components/shoppingListApp.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="inputField">
+     <div class="item">
+       <input type="text" placeholder=" + New Item " v-model="newItem" class="input-text">
+       <button class="addButton" @click="addItem">Add</button>
+     </div>
+     <shoppingListItem 
+      v-for="item in items" 
+      :key="item.id" 
+      :item="item"
+      @removeClicked="removeItem"
+      />
+    </div>
+   </template>
+   
+     <script>
+   import shoppingListItem from './shoppingListItem.vue'
+   export default {
+    components: {
+      shoppingListItem
+    },
+     data() {
+     return {
+       newItem: '',
+         items: [
+         {title: 'Milk', completed: false, id: '1'},
+         {title: 'Eggs', completed: false, id: '2'},
+         {title: 'Butter', completed: false, id: '3'},
+         {title: 'Cheese', completed: true, id: '4'},
+         {title: 'Yoghurt', completed: true, id: '5'}
+         ]
+       }
+     },
+     methods: {
+       addItem() {
+        this.items.push({
+         title: this.newItem, 
+         completed: false,
+         id: Math.random() 
+       })
+       this.newItem = ''
+       },
+       removeItem(id) {
+         const index = this.items.findIndex(el => el.id === id)
+         this.items[index].completed = true
+       }
+     }
+   }
+   </script>
